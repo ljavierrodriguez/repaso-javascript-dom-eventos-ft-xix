5512febafeafa21115b6b5db86a85ddf80d2d937
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-02-17 12:47:44 +0000

    Practica DOM y Eventos

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,29 @@ createElement('tag') // crear un nuevo elemento segun la etiqueta dada
 createTextNode('texto') // crear un nuevo texto segun el valor dado
 */
 
+/* 
+1. Modificar el Texto del Logo y escribir su nombre
+2. Agregar dos opciones al menu principal ejemplo: (login, register)
+3. Modificar el atributo src de la imagen del "Heading Row" y cargar una imagen desde "lorem picsum"
+4. Agregar una parrafo nuevo y asignarles las mismas clases que tiene el parrafo actual y agregarlo antes del mismo
+5. Modificar todos los cards de la seccion content y cambiar su informacion:
+    titulo: Card One por Content 1 y asi cada uno de los titulos.
+Bonus:
+6. Construir una nueva seccion y agregar antes del footer un formulario de contacto con 3 inputs (name, email, message) y un boton de enviar
+7. Crear una funcion que muestre en pantalla (alert) los valores escritos en el formulario al momento de hacer click en el boton (añadir usando "addEventListener").
+
+*/
+
 // Respuesta 1.
+
 // Respuesta 2.
+
 // Respuesta 3.
+
 // Respuesta 4.
+
 // Respuesta 5.
+
 // Respuesta 6.
+
 // Respuesta 7.
