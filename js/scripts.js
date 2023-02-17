e7c0b8489a355b29c26b935e00a2321387344c99
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-02-17 12:42:43 +0000

    Practica DOM y Eventos

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/* 
+---- Metodos de Acceso a los Elementos (nodos) -----
+getElementById('id') // retorna el elemento segun su ID
+getElementsByClassName('class') // retorna una coleccion de elementos segun la clase que contiene
+getElementsByTagName('tag') // retorna una coleccion de elementos segun la etiqueta o tag dado
+getElementsByName('name') // retorna una coleccion de elementos segun el valor del atributo name
+querySelector('selector_css') // retorna el primer elemento con el selector css dado
+querySelectorAll('selector_css') // retorna todos los elementos con el selector css dado
+---- Metodos de Creacion de Elementos (nodos) -----
+createElement('tag') // crear un nuevo elemento segun la etiqueta dada
+createTextNode('texto') // crear un nuevo texto segun el valor dado
+*/
+
+// Respuesta 1.
+// Respuesta 2.
+// Respuesta 3.
+// Respuesta 4.
+// Respuesta 5.
+// Respuesta 6.
+// Respuesta 7.
