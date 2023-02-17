b3720dd16792a1034c39d74b9297701eaacb704a
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-02-17 18:27:59 +0000

    Usando el DOM

--- js/scripts.js
+++ js/scripts.js
@@ -26,14 +26,143 @@ Bonus:
 
 // Respuesta 1.
 
+let brand = document.querySelector('.navbar-brand');
+//brand.innerHTML = "Luis Rodriguez";
+brand.textContent = "Luis Rodriguez";
+
 // Respuesta 2.
+
+let menu = document.querySelector('.navbar-nav');
+
+let navItemLogin = document.createElement('li')
+let navLinkLogin = document.createElement('a');
+navItemLogin.classList.add('nav-item');
+navLinkLogin.classList.add('nav-link');
+navLinkLogin.href = '#!';
+navLinkLogin.innerHTML = "Login";
+navItemLogin.appendChild(navLinkLogin);
+
+let navItemRegister = document.createElement('li')
+let navLinkRegister = document.createElement('a');
+navItemRegister.classList.add('nav-item');
+navLinkRegister.classList.add('nav-link');
+navLinkRegister.href = '#!';
+navLinkRegister.innerHTML = "Register";
+navItemRegister.appendChild(navLinkRegister);
+
+menu.appendChild(navItemLogin);
+menu.appendChild(navItemRegister);
 
 // Respuesta 3.
 
+let img = document.querySelector('img');
+img.src = "https://picsum.photos/id/237/900/400"
+img.classList.add('shadow');
+
 // Respuesta 4.
+
+let newParapraph = document.createElement('p');
+newParapraph.classList.add('text-white', 'm-0');
+newParapraph.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quasi impedit ad repellat blanditiis cupiditate culpa voluptas dolores.'
+//console.log(newParapraph);
+
+let currentParagraph = document.querySelector('p.text-white');
+//console.log(currentParagraph.innerHTML);
+
+let card = document.querySelector('.card.bg-secondary .card-body');
+//console.log(card);
+
+card.insertBefore(newParapraph, currentParagraph); // inserta el elemento antes del elemento indicado
 
 // Respuesta 5.
 
+let titles = document.querySelectorAll('h2.card-title');
+console.log(titles);
+
+for(let i = 0; i < titles.length; i++){
+    titles[i].innerHTML = 'Content ' + (i+1);
+}
+
 // Respuesta 6.
+
+let container = document.querySelector('.container.px-4');
+let rowDiv = document.createElement('div');
+
+rowDiv.classList.add('row', 'gx-4', 'gx-lg-5', 'align-items-center', 'my-5');
+
+let colDiv = document.createElement('div');
+colDiv.classList.add('col-md-12', 'mb-5');
+
+colDiv.innerHTML="Formulario de Contacto";
+
+/* 
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+*/
+
+
+let contactForm = document.createElement('form');
+
+// Campo para el nombre
+let divInputName = document.createElement('div');
+let labelName = document.createElement('label');
+let inputName = document.createElement('input');
+divInputName.classList.add('mb-3');
+labelName.classList.add('form-label');
+labelName.htmlFor = "name";
+labelName.innerHTML = "Name";
+inputName.classList.add('form-control');
+inputName.id = "name";
+inputName.placeholder = "Insert Name";
+inputName.type = "text";
+divInputName.appendChild(labelName);
+divInputName.appendChild(inputName);
+
+// Campo para el email
+let divInputEmail = document.createElement('div');
+let labelEmail = document.createElement('label');
+let inputEmail = document.createElement('input');
+divInputEmail.classList.add('mb-3');
+labelEmail.classList.add('form-label');
+labelEmail.htmlFor = "email";
+labelEmail.innerHTML = "Email"
+inputEmail.classList.add('form-control');
+inputEmail.id = "email";
+inputEmail.placeholder = "Insert Email";
+inputEmail.type = "email";
+divInputEmail.appendChild(labelEmail);
+divInputEmail.appendChild(inputEmail);
+
+
+// Campo Boton para enviar el formulario
+
+let btnSend = document.createElement('button');
+btnSend.classList.add('btn', 'btn-primary', 'btn-sm', 'w-100', 'mb-3')
+btnSend.innerHTML = "Enviar Formulario"
+btnSend.type = "button";
+
+btnSend.addEventListener('click', () => {
+    alert(inputName.value + " " + inputEmail.value);
+})
+
+// Añadimos los elementos al formulario 
+contactForm.appendChild(divInputName);
+contactForm.appendChild(divInputEmail);
+contactForm.appendChild(btnSend);
+
+// añadimos el formulario al documento
+colDiv.appendChild(contactForm);
+
+rowDiv.appendChild(colDiv);
+console.log(rowDiv);
+container.appendChild(rowDiv)
+
+console.log(container);
 
 // Respuesta 7.
